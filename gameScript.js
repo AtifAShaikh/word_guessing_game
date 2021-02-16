73c5f4f62d0f592658f1bfd905bd9d55865223b1
Edward Arana <Edwardaranadlt@gmail.com>
2021-02-16 13:58:20 -0800

    added functions to the js and some comments

---
+++ gameScript.js
@@ -0,0 +1,55 @@
+// Word Guessing Game
+
+ //   1. A list of words for our word guessing game. 
+ //     Var to hold Wins/Losses/TimerValues.
+ //     
+ //     Create a function to start the game, and connect it to the start button.
+ //     Create a function Start the timer randomly pick word from array and display _ _ _ _ _ 
+ 
+ //   2. We need to create an Array [a,b,c,d, ect] where the letters 
+
+var listOfWords = ["dog", "cat", "frog", "bird"];
+var wins = 0;
+var losses = 0;
+var solution = [];
+var answer = [];
+
+//Refs to Win/loss Text and Timer text, and the word text startbutton, restartbutton 
+
+function startGame(){
+    
+}
+
+function setUpWord(){
+
+}
+
+function loseGame(){
+
+}
+
+function winGame(){
+
+}
+
+function startTimer(){
+
+}
+
+function checkKeyboardInput(keyPressed){
+
+}
+
+function listifyString(stringToListify){
+
+}
+
+function displayAnswer(){
+
+}
+
+function restartGame(){
+
+}
+
+// keypressdown that executes the checkKeyboardInput with said key
